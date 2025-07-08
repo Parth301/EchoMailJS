5d29c9b4a1189ddad7ed49b16337ea66c22f3558
Parth <127748701+Parth301@users.noreply.github.com>
2025-07-08 14:43:27 +0530

    Update verifyToken.js

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,8 +1,8 @@
 // /api/utils/verifyToken.js
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
-module.exports = function (req, res, next) {
+export default function verifyToken(req, res, next) {
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) return res.status(401).json({ error: 'Unauthorized' });
@@ -14,5 +14,4 @@ module.exports = function (req, res, next) {
   } catch (err) {
     res.status(401).json({ error: 'Invalid token' });
   }
-
-};
+}
